6e90bb1c1c711f7cfaaf161642add0611e7bf412
uditkr27 <utkmr27jun@gmail.com>
2020-05-18 03:50:12 +0530

    api

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import React from 'react';
 
 import {Cards, Chart, CountryPicker} from './components';
 import style from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
+
+   async componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+ 
+
 render() {
     return (
         <div className ={style.container}>
